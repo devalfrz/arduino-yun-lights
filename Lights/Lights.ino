186785cc1a1d7708174c89217d271b95275ab2b7
Alfredo Rius <alfredo.rius@gmail.com>
2016-01-29 20:14:27 -0600

    Made JSON response

--- Lights/Lights.ino
+++ Lights/Lights.ino
@@ -3,6 +3,8 @@
 #include <YunClient.h> 
  
 YunServer server;
+String response;
+
  
 void setup() {
   Bridge.begin();
@@ -13,21 +15,18 @@ void setup() {
  
 void loop() {
   YunClient client = server.accept();
- 
   if (client) {
     String command = client.readString();
     command.trim();
-    if (command == "status"){
-      client.print(digitalRead(11));
-    }
-    else if (command == "lights-on") {
+    
+    if (command == "lights-on") {
       digitalWrite(11,LOW);
-      client.print(digitalRead(11));
     }
     else if (command == "lights-off") {
       digitalWrite(11,HIGH);
-      client.print(digitalRead(11));
     }
+    response = String(digitalRead(11));
+    client.print(String("{\"state\":"+response+"}"));
     client.stop();
   }
  
